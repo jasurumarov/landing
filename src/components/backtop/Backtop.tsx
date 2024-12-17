3c04968d699be532c1c77495f3a2b225b2413e72
jasurumarov <jasurumarov199@gmail.com>
2024-12-17 15:09:58 +0500

    sth changed

--- src/components/backtop/Backtop.tsx
+++ src/components/backtop/Backtop.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import { FaAngleUp } from 'react-icons/fa'
-import { Link } from 'react-router-dom';
 
 const Backtop: React.FC = () => {
     const [visible, setVisible] = useState(false);
